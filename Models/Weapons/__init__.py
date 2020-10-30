c4d3326acb6653b0dbc02408dba219ca0f81fc8a
牧瀬紅莉栖 <MakiseKurisu@users.noreply.github.com>
2020-10-30 18:31:19 -0500

    Update attack estimation based on lv90 Skyward Blade

--- Models/Weapons/__init__.py
+++ Models/Weapons/__init__.py
@@ -4,11 +4,15 @@ class Weapon(Model):
     def __init__(self, lv1_attack):
         super().__init__()
         if lv1_attack == 41:
-            self.attack_base += 475
+            self.attack_base += 542
         elif lv1_attack == 42:
-            self.attack_base += 532
+            self.attack_base += 555
         elif lv1_attack == 44:
-            self.attack_base += 580
+            self.attack_base += 582
+        elif lv1_attack == 46:
+            # verified
+            # energy_recharge 0.12->0.551
+            self.attack_base += 608
         else:
             raise ValueError(f"Unknown weapon level 1 attack {lv1_attack}.")
 
